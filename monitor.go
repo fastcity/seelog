920c94601bbecdc9521c77319818c7d6e34fbd7c
kangxiaodong <kangxiaodong@sayinfo.cn>
2018-08-03 10:57:14 +0800

    avoid nil-pointer and mermory leak in monitor

--- monitor.go
+++ monitor.go
@@ -1,45 +1,46 @@
 package seelog
 
 import (
+	"log"
 	"os"
 	"time"
-	"log"
 )
 
-
 // 监控日志文件
-func monitor(filePath string){
+func monitor(filePath string) {
 	defer func() {
-		if err := recover();err != nil{
-			log.Printf("[seelog] error:%+v",err)
+		if err := recover(); err != nil {
+			log.Printf("[seelog] error:%+v", err)
 		}
 	}()
 
-	fileInfo,err := os.Stat(filePath)
+	fileInfo, err := os.Stat(filePath)
 	if err != nil {
-		log.Printf("[seelog] error:%v",err.Error())
+		log.Printf("[seelog] error:%v", err.Error())
 	}
 	offset := fileInfo.Size()
 	for {
-		fileInfo,_ = os.Stat(filePath)
+		fileInfo, _ = os.Stat(filePath)
 		newOffset := fileInfo.Size()
 		if offset < newOffset {
-			msg := make([]byte,newOffset - offset)
-			file,err := os.Open(filePath)
+			msg := make([]byte, newOffset-offset)
+			file, err := os.Open(filePath)
 			if err != nil {
-				log.Printf("[seelog] error:%v",err.Error())
+				log.Printf("[seelog] error:%v", err.Error())
+				continue
 			}
-			_,err = file.Seek(offset,0)
+			_, err = file.Seek(offset, 0)
 			if err != nil {
-				log.Printf("[seelog] error:%v",err.Error())
+				log.Printf("[seelog] error:%v", err.Error())
 			}
 
-			_,err = file.Read(msg)
+			_, err = file.Read(msg)
 			if err != nil {
-				log.Printf("[seelog] error:%v",err.Error())
+				log.Printf("[seelog] error:%v", err.Error())
 			}
 			manager.broadcast <- msg
 			offset = newOffset
+			file.Close()
 		}
 		time.Sleep(200 * time.Millisecond)
 	}
